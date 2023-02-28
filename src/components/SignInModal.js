438b365144d687f8f72fa411f0175670c0dfe634
Warrios974 <kenny.lauret@outlook.fr>
2023-02-28 19:56:49 +0400

    Ajouter dans icons dans la navbar

--- src/components/SignInModal.js
+++ src/components/SignInModal.js
@@ -1,5 +1,10 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React, { useContext } from 'react'
 import { ModalContext } from '../contexts/modalContext'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faXmark } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faXmark)
 
 function SignInModal() {
 
@@ -13,7 +18,11 @@ function SignInModal() {
         <div className='modal__containe'>
             <div className='modal__header'>
               <h5>Se connecter</h5>
-              <button onClick={() => toggleModals("close")}>Fermer</button>
+              <button 
+              className='btn btn__close'
+                onClick={() => toggleModals("close")}>
+                <FontAwesomeIcon icon="fa-solid fa-xmark" size='lg'/>
+              </button>
             </div>
             <div className='modal__body'>
               <button>Me connecter avec Google</button>
