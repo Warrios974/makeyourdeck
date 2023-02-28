98d345ab61f90a54c0de641b9f370eee30a54dea
Warrios974 <kenny.lauret@outlook.fr>
2023-02-28 19:55:15 +0400

    Suppression de la modal dans Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import SignInModal from '../components/SignInModal'
 
 function Home() {
   return (
     <main className='main'>
       <h1>Bienvenue sur Make your deck</h1>
-      <SignInModal />
     </main>
   )
 }
