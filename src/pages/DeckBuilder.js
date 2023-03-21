c42c319f80755b8514c4b05412a2a05ed0c3a397
Warrios974 <kenny.lauret@outlook.fr>
2023-03-21 17:21:29 +0400

    Update Component function DeckBuilder

--- src/pages/DeckBuilder.js
+++ src/pages/DeckBuilder.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getCardsByColors } from '../api/MagicApi'
+import Carrousel from '../components/deckBuilder/Carrousel'
 import SearchAndFilterDeckForm from '../components/forms/searchAndFilterForm/SearchAndFilterDeckForm'
 
 function DeckBuilder() {
 
+    const [currentCards, setCurrentCards] = useState([])
+    const [nextPage, setnextPage] = useState()
+
+    useEffect(() => {
+        const initCards = async () => {
+            const cards = await getCardsByColors('white')
+            setCurrentCards([...cards.data])
+            setnextPage(cards.next_page)
+            return cards
+        }
+        initCards()
+    }, [])
+    
+    if (currentCards === []) return <main className='main'>Loading...</main>
+
     return (
         <main className='main'>
-            <SearchAndFilterDeckForm />
+            <SearchAndFilterDeckForm 
+                setCurrentCards={setCurrentCards}/>
+            <Carrousel
+                nextPage={nextPage}
+                currentCards={currentCards} />
         </main>
     )
 }
