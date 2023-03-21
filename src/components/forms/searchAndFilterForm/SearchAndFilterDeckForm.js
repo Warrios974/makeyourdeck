7b892129cb93c2fb42258e6487eed1bcbf63845e
Warrios974 <kenny.lauret@outlook.fr>
2023-03-21 17:20:55 +0400

    Update Component SearchAndFilterForm

--- src/components/forms/searchAndFilterForm/SearchAndFilterDeckForm.js
+++ src/components/forms/searchAndFilterForm/SearchAndFilterDeckForm.js
@@ -1,16 +1,68 @@
 import React from 'react'
+import { ReactComponent as WhiteManaSvg } from "../../../assets/icons/mtg/W.svg";
+import { ReactComponent as BlueManaSvg } from "../../../assets/icons/mtg/U.svg";
+import { ReactComponent as RedManaSvg } from "../../../assets/icons/mtg/R.svg";
+import { ReactComponent as GreenManaSvg } from "../../../assets/icons/mtg/G.svg";
+import { ReactComponent as BlackManaSvg } from "../../../assets/icons/mtg/B.svg";
+import { getCardsByColors } from '../../../api/MagicApi';
 
-function SearchAndFilterForm() {
+function SearchAndFilterForm(props) {
+
+  const { setCurrentCards } = props
+
+  const handleClickFilter = async (e,filter) => {
+    e.preventDefault()
+    const cards = await getCardsByColors(filter)
+    setCurrentCards([...cards.data])
+  }
+    
   return (
-    <form className='form searchAndFilterDeck'>
-      <fieldset>
+    <form className='form formSearchAndFilterDeck'>
+      <fieldset className='formSearchAndFilterDeck__search'>
         <label>Chercher</label>
         <input formEncType='texte'></input>
       </fieldset>
-      <fieldset>
+      <fieldset className='formSearchAndFilterDeck__filter'>
         <ul>
           <li>
-
+            <button 
+            className='btn btn__mana'
+            onClick={(e) => handleClickFilter(e,'white')}
+            >
+              <WhiteManaSvg />
+            </button> 
+          </li>
+          <li>
+            <button 
+            className='btn btn__mana'
+            onClick={(e) => handleClickFilter(e,'blue')}
+            >
+              <BlueManaSvg />
+            </button> 
+          </li>
+          <li>
+            <button 
+            className='btn btn__mana'
+            onClick={(e) => handleClickFilter(e,'red')}
+            >
+              <RedManaSvg />
+            </button> 
+          </li>
+          <li>
+            <button 
+            className='btn btn__mana'
+            onClick={(e) => handleClickFilter(e,'green')}
+            >
+              <GreenManaSvg />
+            </button> 
+          </li>
+          <li>
+            <button 
+            className='btn btn__mana'
+            onClick={(e) => handleClickFilter(e,'black')}
+            >
+              <BlackManaSvg />
+            </button> 
           </li>
         </ul>
       </fieldset>
