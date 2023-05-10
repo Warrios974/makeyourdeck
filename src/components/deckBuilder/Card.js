bc9d5b2c34c4c7f10d3352fa17dcfb5e64860388
Warrios974 <kenny.lauret@outlook.fr>
2023-05-10 17:46:47 +0400

    Modif card et carrousel

--- src/components/deckBuilder/Card.js
+++ src/components/deckBuilder/Card.js
@@ -6,11 +6,27 @@ function Card(props) {
       
     const fatoryCard = (layout) => {
 
-      if (layout === 'normal') {
+      const conditionDoubleFaces = layout === 'transform' || layout === 'modal_dfc' || layout === 'double_faced_token' || layout === 'art_series ' || layout === 'reversible_card '
+      const conditionSingleFaces = layout !== 'transform' || layout !== 'modal_dfc' || layout !== 'double_faced_token' || layout !== 'art_series ' || layout !== 'reversible_card '
+
+      if (conditionDoubleFaces) {
+
+        const carddoubleFaces = card.card_faces
+
         return (
-          <article key={`${card.id}`}>
-              <img src={card.image_uris['small']} alt={card.name} />
-          </article>
+          <div className='card__doubleFaces'>
+            { carddoubleFaces.map((card) => (
+                <img src={card.image_uris['normal']} alt={card.name} key={`${card.name}`}  />
+              ))
+            }
+          </div>
+        )
+      }
+      if (conditionSingleFaces) {
+        return (
+          <>
+              <img src={card.image_uris['normal']} alt={card.name} />
+          </>
         )
       }
 
--- src/components/deckBuilder/Carrousel.js
+++ src/components/deckBuilder/Carrousel.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import Card from './Card'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+library.add(faChevronLeft, faChevronRight)
 
 function Carrousel(props) {
     
@@ -7,20 +12,20 @@ function Carrousel(props) {
 
   if (currentCards === []) return <div>Loading...</div>
 
-  console.log('====');
-  console.log('currentCards',currentCards);
-  console.log('====');
-
   return (
-    <div>
-      <h2>Carrousel</h2>
-      { currentCards.slice(0, 5).map((card) => (
-          <div key={ card.id }>
-              <Card
-              card={card}/>
-          </div>
+    <div className='carrousel'>
+      <FontAwesomeIcon 
+        className='carrousel__arrow carrousel__arrow--left'
+        icon="fa-solid fa-chevron-left" />
+      { currentCards.slice(0, 10).map((card) => (
+          <article key={`${card.id}`} className='card'>
+            <Card card={card}/>
+          </article>
         ))
       }
+      <FontAwesomeIcon
+        className='carrousel__arrow carrousel__arrow--right' 
+        icon="fa-solid fa-chevron-right" />
     </div>
   )
 }
