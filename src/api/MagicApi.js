278bab17e51240b8bf9a83463fae186e8e1f9fae
Warrios974 <kenny.lauret@outlook.fr>
2023-05-10 17:42:48 +0400

    Ajout des fonction API

--- src/api/MagicApi.js
+++ src/api/MagicApi.js
@@ -14,3 +14,55 @@ export const getCardsByColors = async (filter) => {
   return data
 
 }
+
+export const getCards = async (URI) => {
+
+  const call = await fetch(URI)
+
+  /*.then((response) => response.json())
+  .then((data) => {
+    console.log("Success:", data);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });*/
+  const data = await call.json()
+
+  return data
+
+}
+
+export const getAutocomplete = async (value) => {
+
+  const uri = 'https://api.scryfall.com/cards/autocomplete?q='
+
+  const url = uri + value
+
+  const call = await fetch(url)
+
+  /*.then((response) => response.json())
+  .then((data) => {
+    console.log("Success:", data);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });*/
+  const data = await call.json()
+
+  return data
+
+}
+
+export const initSortCards = (data) => {
+  
+  if (data && data.length > 2) {
+
+    const sortData = data.sort((a, b) => a.cmc - b.cmc)
+    
+    return sortData
+
+  }
+
+  return data
+
+}
