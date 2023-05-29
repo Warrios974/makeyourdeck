7af4bc9d69dd078a9206d455f0db92dd94a93921
Kenny Lauret <kenny.lauret@outlook.fr>
2023-05-29 10:43:03 +0400

    Mise en place de Bootstrap react pour la modale

--- src/components/SignInModal.js
+++ src/components/SignInModal.js
@@ -3,6 +3,7 @@ import React, { useContext } from 'react'
 import { ModalContext } from '../contexts/modalContext'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faXmark } from '@fortawesome/free-solid-svg-icons'
+import { Button, Modal } from 'react-bootstrap'
 
 library.add(faXmark)
 
@@ -14,25 +15,20 @@ function SignInModal() {
     <>
     {
     modalState.signInModal === true &&
-      <div className='modal'>
-        <div className='modal__containe'>
-            <div className='modal__header'>
-              <h5>Se connecter</h5>
-              <button 
-              className='btn btn__close'
-                onClick={() => toggleModals("close")}>
-                <FontAwesomeIcon icon="fa-solid fa-xmark" size='lg'/>
-              </button>
-            </div>
-            <div className='modal__body'>
-              <button>Me connecter avec Google</button>
-            </div>
-        </div>
-        <div 
-          className='modal__overlay' 
-          onClick={() => toggleModals("close")}>
-        </div>
-      </div>
+    <div className='modal show' style={{display : 'block'}}>
+      <Modal.Dialog>
+        <Modal.Header>
+          <Modal.Title>Se connecter</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>Connecter vous</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => toggleModals("close")}>Close</Button>
+          <Button variant="primary">Me connecter avec Google</Button>
+        </Modal.Footer>
+      </Modal.Dialog>
+    </div>
     }
     </>
   )
