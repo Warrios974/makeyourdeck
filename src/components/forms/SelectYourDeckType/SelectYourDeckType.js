15635d63961fe2c0dd54f7d347647ec4dc2c4e51
Kenny Lauret <kenny.lauret@outlook.fr>
2023-06-08 17:28:44 +0400

    Mis en place de react hook form

--- src/components/forms/SelectYourDeckType/SelectYourDeckType.js
+++ src/components/forms/SelectYourDeckType/SelectYourDeckType.js
@@ -1,147 +1,128 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext } from 'react'
 import { Button, Form, Modal } from 'react-bootstrap'
-import { ModalContext } from '../../../contexts/modalContext'
 import { DeckBuilderContext } from '../../../contexts/deckBuilderContext'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
 
-function SelectYourDeckType(props) {
+const schema = yup.object({
+  name: yup.string().matches(/^[a-zA-Z0-9 ]+$/i, 'Is not in correct format').min(3).required(),
+  format: yup.string().matches(/^commander$|^vintage$|^standard$|^modern$|^brawl$|^duel$/i, 'Not conforme').required(),
+  isPublic: yup.boolean(),
+}).required();
+
+function SelectYourDeckType() {
+
+  const { register, handleSubmit, formState : { errors }, formState } = useForm({
+    mode: 'onTouched',
+    resolver: yupResolver(schema)
+  })
+
+  const { isSubmitting, isSubmitSuccessful } = formState
 
   const { stateFilters, stateDeck } = useContext(DeckBuilderContext)
   
   const [filters, setFilters] = stateFilters
   const [currentDeck, setCurrentDeck] = stateDeck
 
-  const [nameDeck, setNameDeck] = useState(undefined)
-  const [nameDeckError, setNameDeckError] = useState({
-    error : false,
-    textError : null
-  })
-  const [gameFormat, setGameFormat] = useState(undefined)
-  const [gameFormatError, setGameFormatError] = useState({
-    error : false,
-    textError : null
-  })
+  const formatsList = ['standard','commander', 'modern', 'vintage', 'brawl','duel']
 
-  const formatsList = [
-    'standard','commander', 'modern', 'vintage', 'brawl'
-  ]
-  
-  useEffect(() => {
-    const initComponent = async () => {
+  const onSubmit = (data) => {
 
-        return fetch
-    }
-    initComponent()
-}, [])
-
-  const handleSubmit = (e) => {
-
-    e.preventDefault()
-
-    const name = nameDeck
-    const format = gameFormat
-
-    var regex = new RegExp("^[a-zA-Z0-9 ]+$");
-
-    if (name === '' || name === undefined) {
-
-      setNameDeckError({
-        error : true,
-        textError : "The name can't be void"
-      })
-
-      return false
-    }
-
-    if (regex.test(name) === false) {
-
-      setNameDeckError({
-        error : true,
-        textError : "The can't containe special caracteres"
-      })
-
-      return false
-    }
-
-    if (name || name !== undefined) {
-
-      setNameDeckError({
-        error : false,
-        textError : null
-      })
-
-    }
-    
-    if (format === undefined || format === 'Select your format') {
-
-      setGameFormatError({
-        error : true,
-        textError : "Select a format please"
-      })
-
-      return false
-    }
-    
-    if (format !== undefined || format !== 'Select your format') {
-
-      let isIntheList = false
-
-      formatsList.forEach(elem => {
-        if (format === elem) {
-          isIntheList = true
-        }
-      });
-
-      if (isIntheList) {
-        setGameFormatError({
-          error : false,
-          textError : null
-        })
-      }
-
-      if (!isIntheList) {
-        setGameFormatError({
-          error : true,
-          textError : "This format isn't in the list"
-        })
-
-        return false
-      }
-    }
+    const name = data.name
+    const format = data.format
+    const isPublic = data.isPublic
 
     let localFilters = JSON.parse(JSON.stringify(filters))
     localFilters.formats[format] = true
 
     let localDeck = JSON.parse(JSON.stringify(currentDeck))
     localDeck.name = name
-    localDeck.isInit = true
+    localDeck.type = format
+    localDeck.public = isPublic
+    
+    if (!localDeck.isInit && localDeck.type !== null) {
+
+      const sixty = ['vintage','standard','modern']
+      const hundred = ['commander','brawl','duel']
+
+      const isSixty = sixty.filter((element) => element === format)
+      const isHundred = hundred.filter((element) => element === format)
+
+      if (isSixty) {
+
+          let localCards = {
+              mainDeck : [{total : 60}],
+              reserve : [{total : 15}],
+              numberExemple : 4
+          } 
+
+          localDeck.cards = localCards
+      }
+
+      if (isHundred) {
+
+          let localCards = {
+              commander : [],
+              mainDeck : [{total : 100}],
+              reserve : null,
+              numberExemple : 1
+          } 
+
+          localDeck.cards = localCards
+
+      }
+
+      localDeck.isInit = true
+
+  }
 
     setFilters(localFilters)
     setCurrentDeck(localDeck)
 
   }
 
-  if(!currentDeck.isInit) return  (
+  if(!currentDeck.isInit && !isSubmitSuccessful) return  (
     <div className='modal show' style={{display : 'block'}}>
-      <Form onSubmit={(e) => handleSubmit(e)}>
+      <Form onSubmit={handleSubmit(onSubmit)}>
         <Modal.Dialog>
           <Modal.Header>
             <Modal.Title>Init your deck</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-              <Form.Label>Deck name</Form.Label>
-              <Form.Control type="text" placeholder="Gruul Aggro" required onChange={(e) => setNameDeck(e.target.value)} className='my-3'/>
-                <span>{nameDeckError.textError}</span>
-              <Form.Select aria-label="Default select example" required onChange={(e) => setGameFormat(e.target.value)} className='my-3'>
-                <option>Select your format</option>
-                  { formatsList.map((format) => (
-                      <option key={format} value={format}>{format}</option>
-                    ))
-                  }
-              </Form.Select>
-                <span>{gameFormatError.textError}</span>
+            <Form.Label>Deck name</Form.Label>
+            <Form.Control 
+              type="text" 
+              placeholder="Gruul Aggro"
+              className='my-3'
+              name='Name'
+              {...register('name')}
+              defaultValue={'Gruul Agro'}
+              />
+              {errors.name && <span>{errors.name.message}</span>}
+            <Form.Select 
+              aria-label="Default select example" 
+              className='my-3'
+              {...register('format')}
+              defaultValue={'standard'}
+              >
+              <option>Select your format</option>
+                { formatsList.map((format) => (
+                    <option key={format} value={format}>{format}</option>
+                  ))
+                }
+            </Form.Select>
+              {errors.format && <span>{errors.format.message}</span>}
+            <Form.Check // prettier-ignore
+              type="switch"
+              id="plublic-or-private"
+              label="Public"
+              {...register('isPublic')}
+              defaultChecked={true}
+            />
           </Modal.Body>
           <Modal.Footer>
-            <Button variant="primary" type="submit">Create</Button>
+            <Button disabled={isSubmitting} variant="primary" type="submit">Create</Button>
           </Modal.Footer>
         </Modal.Dialog>
       </Form>
