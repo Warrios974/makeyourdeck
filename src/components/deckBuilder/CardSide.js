a2faed454813b0d34d0a99bd957c4929a811e635
Kenny Lauret <kenny.lauret@outlook.fr>
2023-06-13 18:34:08 +0400

    Ajout component card in sidebar

---
+++ src/components/deckBuilder/CardSide.js
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react'
+import { Button } from 'react-bootstrap'
+import { DeckBuilderContext } from '../../contexts/deckBuilderContext'
+import ManaCost from './ManaCost'
+
+function CardSide({ card }) {
+
+    const { stateDeck } = useContext(DeckBuilderContext)
+
+    const [currentDeck, setCurrentDeck, setAddCard, setRemoveCard] = stateDeck
+
+  return (
+    <div>
+    <Button
+        key={`${card.id}`} 
+        onClick={() => setRemoveCard(card)}
+        className='m-2'>
+        
+        <span>{card.quantity}x </span>
+        <span>{card.name}</span>
+        <ManaCost manaCost={card.mana_cost} />
+
+    </Button>
+    </div>
+  )
+}
+
+export default CardSide
--- src/components/deckBuilder/Carrousel.js
+++ src/components/deckBuilder/Carrousel.js
@@ -5,7 +5,6 @@ import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons
 import { Col, Row } from 'react-bootstrap'
 import { DeckBuilderContext } from '../../contexts/deckBuilderContext'
 
-
 library.add(faChevronLeft, faChevronRight)
 
 function Carrousel() {
@@ -14,7 +13,32 @@ function Carrousel() {
     
   const [currentCards] = stateCurrentCards
 
-  if (currentCards.length === 0 || loadingData) return <div>Loading...</div>
+  let rowOne = []
+
+  let rowTwo = []
+  
+  const seperateCards = () => {
+
+    currentCards.forEach((card, index) => {
+      if(index % 2 === 0) {
+        rowTwo.push(card)
+      } else {
+        rowOne.push(card)
+      }
+    });
+
+    return { rowOne, rowTwo }
+  }
+
+  if (currentCards.length === 0 || loadingData) {
+    return (
+      <Col sm={9}>
+        <Row className='overflow-auto' style={{height: "40rem"}}>
+          Loading
+        </Row>
+      </Col>
+    )
+  }
 
   if (currentCards[0].object === "error") {
     return (
@@ -29,10 +53,21 @@ function Carrousel() {
       )
   }
 
+  seperateCards()
+
   return (
-    <Col sm={9}>
-      <Row className='overflow-auto' style={{height: "40rem"}}>
-        { currentCards.map((card) => (
+    <Col sm={9} className='overflow-auto'>
+      <Row className='d-flex flex-row flex-nowrap'>
+        { rowOne.map((card) => (
+            <article key={`${card.id}`} className='col-3'>
+              <Card 
+                card={card}  />
+            </article>
+          ))
+        }
+      </Row>
+      <Row className='d-flex flex-row flex-nowrap'>
+        { rowTwo.map((card) => (
             <article key={`${card.id}`} className='col-3'>
               <Card 
                 card={card}  />
