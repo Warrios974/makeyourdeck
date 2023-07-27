7ddddf23fb815ece167fc8bf8dcb6b511f39a1b5
Kenny Lauret <kenny.lauret@outlook.fr>
2023-07-27 21:08:03 +0400

    Mise en place de la zone de reserve

--- src/components/deckBuilder/ReserveZone.js
+++ src/components/deckBuilder/ReserveZone.js
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Col } from 'react-bootstrap'
+import Card from './Card'
+import { DeckBuilderContext } from '../../contexts/deckBuilderContext'
 
 function ReserveZone() {
-  return (
-    <Col md={2} className='text-center'>
+
+  const { stateCurrentCards, stateNextPage, stateFilters, stateDeck, stateCurrentSelect } = useContext(DeckBuilderContext)
+
+  const [currentSelect, setCurrentSelect] = stateCurrentSelect
+
+  const [currentDeck, setCurrentDeck, setAddCard, setRemoveCard] = stateDeck
+
+  const reserveDeck = currentDeck.cards.reserve !== null && currentDeck.cards.reserve.cards
+
+
+  const handleClick = () => {
+      setCurrentSelect('reserve')
+  }
+
+  if(reserveDeck) return (
+    <Col md={4} className={currentSelect === 'reserve' ? 'border border-dark text-center' : 'text-center'} onClick={() => handleClick()}>
         <h5>ReserveZone</h5>
+          <div>
+            {
+              reserveDeck.map((card, index) => (
+                <Card 
+                  key={`${index}-${card.id}`}
+                  card={card}
+                  />
+              ))
+            }
+          </div>
     </Col>
   )
 }
