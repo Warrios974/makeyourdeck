0fcb8eda007d0cbbdd7dcea620ca7b6ccd7353fe
Kenny Lauret <kenny.lauret@outlook.fr>
2023-08-22 18:06:56 +0400

    Ajout de la modale carte

--- src/contexts/modalContext.js
+++ src/contexts/modalContext.js
@@ -7,33 +7,55 @@ export function ModalContextProvider(props) {
 
     const [modalState, setModalState] = useState({
         signInModal : false,
-        initYourDeckModal : true
+        initYourDeckModal : false,
+        cardInfos : false,
     })
+
+    const [cardInfoData, setCardInfoData] = useState()
 
     //Fermer ou ouvrir les modales
     const toggleModals = modal => {
         if(modal === "signIn"){
             setModalState({
                 signInModal : true,
-                initYourDeckModal : false
+                initYourDeckModal : false,
+                cardInfos : false,
             })
         }
         if(modal === "initYourDeck"){
             setModalState({
                 signInModal : false,
-                initYourDeckModal : true
+                initYourDeckModal : true,
+                cardInfos : false,
+            })
+        }
+        if(modal === "cardInfos"){
+            setModalState({
+                signInModal : false,
+                initYourDeckModal : false,
+                cardInfos : true,
             })
         }
         if(modal === "close"){
             setModalState({
                 signInModal : false,
-                initYourDeckModal : false
+                initYourDeckModal : false,
+                cardInfos : false,
             })
         }
     }
-    
+
     return (
-        <ModalContext.Provider value={{ modalState, toggleModals }}>
+        <ModalContext.Provider value={{ 
+            stateModal : [
+                modalState,
+                toggleModals
+            ],
+            stateModalData : [
+                cardInfoData,
+                setCardInfoData
+            ]
+        }}>
             {props.children}
         </ModalContext.Provider>
     )
